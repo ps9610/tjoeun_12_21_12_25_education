cee94c2141005dc58024936a3f7b27d02a64df8e
ps9610 <ps961001@gmail.com>
2020-12-23 13:57:05 +0900

    3d회전슬라이드_알고리즘_구현

---
+++ 12_23/12_23_프로젝트_3D회전_슬라이드_배열알고리즘/js/slide_3D_rotate.js
@@ -0,0 +1,60 @@
+;(function(window,document,$,undefined){
+
+    //배열 처리
+    //배열 메서드/맨앞에꺼를한칸씩옆으로이동시키는작업
+        //ㄴ>shift()<->unshift(), push(), pop()
+    var imsi = null;
+    var a = [4,3,0,1,2];//배열의가운데숫자가첫번째슬라이드
+        //console.log(a);
+
+        //a.shift();//시프트이동결과/맨앞의배열값삭제됨
+        //console.log(a);
+            //ㄴ>삭제되서 어디로감?/임시(imsi)에 봐관해줌(임시로가게해줌)
+
+            //nextSlide
+            //imsi = a.shift();//시프트이동결과/맨앞의배열값삭제됨/삭제되서 어디로감?/임시(imsi)로가게해줌
+            //a.push(imsi); // imsi에 보관된 배열값을 a에 다시 넣어준다/순서는가장맨뒤에넣어짐/nextSlide의형태를가짐
+            //console.log(a);
+    
+    $(".nextBtn").on({//버튼누를때마다imsi실행됨/한번누르면한번뒤로/두번누르면두번뒤로/이런식으로
+        click : function(){
+
+            imsi = a.shift();
+            a.push(imsi);
+            console.log(a);
+            slide3dMainFn();
+
+        }
+    });
+
+    //prevSlide
+    $(".prevBtn").on({//버튼누를때마다imsi실행됨/한번누르면한번뒤로/두번누르면두번뒤로/이런식으로
+        click : function(){
+
+            imsi = a.pop(imsi);//이게맨뒤의값삭제시켜줌
+            a.unshift(imsi); //얘는 배열값이 뒤에 늘어남/맨뒷배열값을삭제하고/임시에추가시켜줌
+            console.log(a);
+            slide3dMainFn();
+
+        }
+    });
+
+    function slide3dMainFn(){ //css에서구현했던그대로갖다쓴거임
+        $(".slide").eq(a[0]).css({left:-750}).stop().fadeIn(500,function(){//css는 콜백함수 안되니까 콜백함수 구현하려면 페이드인아웃해야함
+            $(this).css({ zIndex:1, transform:"rotate(0deg) scale(.3) perspective(1500px)" }); //transform은 꼭 문자로 묶어주어야함
+        });
+        $(".slide").eq(a[1]).css({left:-500}).stop().fadeIn(500,function(){
+            $(this).css({ zIndex:2, transform:"rotate(0deg) scale(.6) perspective(1500px)" }); 
+        });
+        $(".slide").eq(a[2]).css({left:0}).stop().fadeIn(500,function(){
+            $(this).css({ zIndex:3, transform:"rotate(0deg) scale(1.2) perspective(3000px)" }); 
+        });
+        $(".slide").eq(a[3]).css({left:500}).stop().fadeIn(500,function(){
+            $(this).css({ zIndex:2, transform:"rotate(0deg) scale(.6) perspective(1500px)" }); 
+        });
+        $(".slide").eq(a[4]).css({left:750}).stop().fadeIn(500,function(){
+            $(this).css({ zIndex:1, transform:"rotate(0deg) scale(.3) perspective(1500px)" }); 
+        })
+        //1.CSS그대로갖다써/2.eq에배열넣어/배열값전달받게
+    }
+})(window,document,jQuery);
